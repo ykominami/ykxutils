bfbd151bb4fd72cf6e53fdd98d2c8061ff8e1edc
ykominami <ykominami@gmail.com>
2022-09-15 08:22:14 +0900

    fix func_git_log

--- lib/ykxutils/gitcmd.rb
+++ lib/ykxutils/gitcmd.rb
@@ -7,26 +7,36 @@ module Ykxutils
     line_no = 1
     buf = []
     prog = "git log --oneline"
-    _stdin, stdout, _stderr = Open3.popen3(prog)
+    _stdin, stdout, stderr = Open3.popen3(prog)
     stdout.each do |line|
       buf << line
       break if line_no == target_line
 
       line_no += 1
     end
+    stderr.each do |line_e|
+      puts line_e
+    end
 
     buf
   end
 
   def func_get_commit_id(target)
+    commit_id = ""
     buf = func_git_log(target)
     index = target - 1
-    buf[index].split.first
+    str = buf[index]
+    if str
+      commit_id = str.split.first
+    end
+    commit_id
   end
 
   def func_git_diff_tree(commit_id)
-    prog = "git diff-tree --name-status -r #{commit_id}"
     buf = []
+    return buf if commit_id == ""
+
+    prog = "git diff-tree --name-status -r #{commit_id}"
     _stdin, stdout, _stderr = Open3.popen3(prog)
     stdout.each do |line|
       buf << line
