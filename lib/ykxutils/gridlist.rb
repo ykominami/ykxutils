0202f6dea77b104e5929bf0cc55d9ef941b86c0a
ykominami <ykominami@gmail.com>
2023-12-12 19:47:44 +0900

    fix for rspec

--- lib/ykxutils/gridlist.rb
+++ lib/ykxutils/gridlist.rb
@@ -1,37 +1,34 @@
-require 'ykxutils/erubyx'
+require "ykxutils/erubyx"
 
 module Ykxutils
   module Gridlist
-    TAMPLETES = { :GRID_DEF =>
-      { :TEMPLATE => %!
+    TAMPLETES = { GRID_DEF: { TEMPLATE: %(
     .g-<%= row %>-<%= colum %> {
       grid-row-start: <%= row %>;
       grid-row-end: ,<%= row + 1 %>;
       grid-column-start: <%= colum %>;
       grid-column-end: <%= colum + 1 %>;
     }
-    !, :OBJ => nil } }
+    ), OBJ: nil } }.freeze
 
     module_function
 
     def make_one_grid(hash, row, colum)
-      unless hash[:OBJ]
-        hash[:OBJ] = Tilt::ErubiTemplate.new { hash[:TEMPLATE] }
-      end
+      hash[:OBJ] = Tilt::ErubiTemplate.new { hash[:TEMPLATE] } unless hash[:OBJ]
       hash[:OBJ].render(Object.new, { row: row, colum: colum })
     end
 
     def make_grid(template_hash, scope, min_row, max_row, min_colum, max_colum)
-      (min_row..max_row).map { |row|
-        (min_colum..max_colum).map { |colum|
+      (min_row..max_row).map do |row|
+        (min_colum..max_colum).map do |colum|
           # puts "#{row} #{colum}"
           value_hash = { row: row, colum: colum }
           # make_one_grid(template_hash, value_hash)
           # p template_hash
           # p value_hash
-          Ykxutils::Erubyx::erubi_render(template_hash, scope, value_hash)
-        }
-      }
+          Ykxutils::Erubyx.erubi_render(template_hash, scope, value_hash)
+        end
+      end
     end
 
     def make_grid_list(min_row, max_row, min_colum, max_colum)
